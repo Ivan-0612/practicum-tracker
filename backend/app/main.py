e1a81716b93a348d74b32a80b251b5b4855aa691
Ivan Benito <ivanbenitosanchez@gmail.com>
2026-03-31 14:44:31 +0200

    errores varios

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth.router)
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["Autenticación"])
 app.include_router(alumnos.router)
 app.include_router(admin.router)
 app.include_router(profesores.router)
